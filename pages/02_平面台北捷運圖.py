ff70767d96cb580e9b843ff82b6d214035bd856f
mo3www <linamy3893451@gmail.com>
2025-11-11 09:16:43 +0000

    deleted:    pages/01-1_taipeiMRT.py 	new file:   "pages/02_\345\271\263\351\235\242\345\217\260\345\214\227\346\215\267\351\201\213\345\234\226.py"

---
+++ pages/02_平面台北捷運圖.py
@@ -0,0 +1,56 @@
+import solara
+import leafmap.maplibregl as leafmap
+
+
+def create_map():
+
+    m = leafmap.Map(
+        style="dark-matter",
+        projection="globe",
+        height="750px",
+        center=[121.5654, 25.0330],
+        zoom=4,
+        sidebar_visible=True,
+    )
+
+    # building_pmtiles = "https://overturemaps-tiles-us-west-2-beta.s3.amazonaws.com/2025-04-23/buildings.pmtiles"
+    road_pmtiles = "https://github.com/leoluyi/taipei_mrt/blob/master/routes.geojson"
+    # building_style = {
+    #     "layers": [
+    #         {
+    #             "id": "Buildings",
+    #             "source": "buildings",
+    #             "source-layer": "building",
+    #             "type": "line",
+    #             "paint": {
+    #                 "line-color": "#ff0000",
+    #                 "line-width": 1,
+    #             },
+    #         },
+    #     ]
+    # }
+    road_style = {
+        "layers": [
+            {
+                "id": "Roads",
+                "source": "transportation",
+                "source-layer": "segment",
+                "type": "line",
+                "paint": {
+                    "line-color": "#ffffff",
+                    "line-width": 2,
+                },
+            },
+        ]
+    }
+    # m.add_pmtiles(
+    #     building_pmtiles, style=building_style, tooltip=True, fit_bounds=False
+    # )
+    m.add_pmtiles(road_pmtiles, style=road_style, tooltip=True, fit_bounds=False)
+    return m
+
+
+@solara.component
+def Page():
+    m = create_map()
+    return m.to_solara()
